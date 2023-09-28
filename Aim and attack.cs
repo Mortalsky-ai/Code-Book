749f82d4a61792c9e4ee31c20f68ecb584dda0ff
Kunal Shikhar <kunalshikhar2@gmail.com>
2023-09-28 16:23:21 +0530

    optimized

--- Aim and attack.cs
+++ Aim and attack.cs
@@ -11,14 +11,13 @@ public class Aimandattack : MonoBehaviour
         return aim_angle;
     } 
 
-    public Rigidbody2D Long_fire(Rigidbody2D bullet, Vector3 bullet_offset, Quaternion rotation, Transform parent, float aim_angle, float bullet_speed) // use to  fire anything
+    public void Long_fire(Rigidbody2D bullet, Vector3 bullet_offset, Quaternion rotation, Transform parent, float aim_angle, float bullet_speed) // use to  fire anything
     {
         Rigidbody2D new_bullet = Instantiate(bullet, parent.position+bullet_offset, rotation, parent);
         new_bullet.velocity = new Vector2(bullet_speed * Mathf.Cos(aim_angle), bullet_speed * Mathf.Sin(aim_angle));
-        return new_bullet; // Store this return in a list to take further actions on these bullets like the rotate with velocity as below
     }
 
-    public void Rotate_with_velocity(Rigidbody2D bullet) // use to rotate arrow in place of bullet in direction of projectile
+    public void Rotate_with_velocity(Rigidbody2D bullet) // use to rotate arrow in place of bullet in direction of projectile, need to attach this script in arrow prefab
     {
         float velocity_angle = Mathf.Atan2(bullet.velocity.y, bullet.velocity.x) * Mathf.Rad2Deg;
         bullet.rotation = velocity_angle;
