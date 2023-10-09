953ed76f36944315bcff27e31629f7349bae8b70
AryanVerma212 <aryanverma212@outlook.com>
2023-10-09 21:49:28 +0530

    Added more savable parameters

--- GameManager.cs
+++ GameManager.cs
@@ -4,6 +4,9 @@ public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
     public Vector2 lastCheckpointPos;
+    public int score;
+    public int lives;
+    public int health;
     private void Awake()
     {
         if (instance == null)
